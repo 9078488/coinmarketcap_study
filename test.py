6a25d66029e3e5e0e94104239f1f6715106322b2
9078488 <9078488@protonmail.com>
2020-05-20 15:57:24 +0800

    2020/05/20

---
+++ test.py
@@ -0,0 +1,21 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+          'start':'1',
+            'limit':'5000',
+              'convert':'USD'
+              }
+headers = {
+          'Accepts': 'application/json',
+            'X-CMC_PRO_API_KEY': '5fa53146-23a7-450f-ad6d-eef06dfdb897',
+            }
+session = Session()
+session.headers.update(headers)
+try:
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)
+    print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+    print(e)
