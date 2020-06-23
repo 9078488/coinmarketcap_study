252f9a3aed3da36be571c8e705c140d9ab03f94e
9078486 <30795091+9078486@users.noreply.github.com>
2020-06-23 12:50:17 +0800

    2020/06/23

--- get_top20_from_top100.py
+++ get_top20_from_top100.py
@@ -10,12 +10,14 @@ original_top_20_2020_06_19 = []
 
 cursor.execute(sql)
 results = cursor.fetchall()
-for item in results[7][1:21]:
+# 如下result加1
+for item in results[8][1:21]:
 	print(item)
 	original_top_20_2020_06_19.append(item.split('_')[1])
 	print('*' * 100)
 
-original_top_20_2020_06_19.insert(0, results[7][1].split('_')[4][0:10])
+# 如下result加1
+original_top_20_2020_06_19.insert(0, results[8][1].split('_')[4][0:10])
 print(original_top_20_2020_06_19)
 
 db.close()
